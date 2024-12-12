5e9e59e9e85d528b2fc37fe71fbfe027bed46016
ju2pter <julianaoliveiravaz93@gmail.com>
2024-12-12 16:49:28 -0300

    Adicionando arquivos do site

--- js/script.js
+++ js/script.js
@@ -1,28 +1,12 @@
-const output = document.getElementById('output');
-const input = document.getElementById('terminal-input');
+// scripts.js
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('section');
+    sections.forEach((section) => {
+        section.classList.add('hidden');
+    });
 
-const commands = {
-    help: "Available commands: <span style='color: #00ff00;'>help</span>, <span style='color: #00ff00;'>about</span>, <span style='color: #00ff00;'>clear</span>",
-    about: "Juliana's Cyber Lab is a place for exploring cybersecurity and backend development.",
-    clear: ""
-};
-
-input.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        const command = input.value.trim();
-        executeCommand(command);
-        input.value = '';
+    const activeSection = document.getElementById(sectionId);
+    if (activeSection) {
+        activeSection.classList.remove('hidden');
     }
-});
-
-function executeCommand(command) {
-    if (commands[command]) {
-        output.innerHTML += `<div>${command}: ${commands[command]}</div>`;
-        if (command === "clear") {
-            output.innerHTML = "";
-        }
-    } else {
-        output.innerHTML += `<div>${command}: Command not found</div>`;
-    }
-    output.scrollTop = output.scrollHeight;
 }
